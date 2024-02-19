515c8724f0b3f85a7001d82b7627cfa402b337aa
HKK <heinkaungkhant1999@gmail.com>
2024-02-19 14:22:17 +0700

    Extract API Key from code base

--- app/ui/lib/chat-data.js
+++ app/ui/lib/chat-data.js
@@ -4,39 +4,4 @@ export const chatData = [
     speaker: 'bot',
     message: 'Hi, How can I assit you today?',
   },
-  {
-    time: 'Sat Feb 17 2024 18:37:47 GMT+0700 (Indochina Time)',
-    speaker: 'user',
-    message: 'I need some help!',
-  },
-  {
-    time: 'Sat Feb 17 2024 18:38:20 GMT+0700 (Indochina Time)',
-    speaker: 'bot',
-    message: 'Sure, let me know!',
-  },
-  {
-    time: 'Sat Feb 17 2024 18:39:29 GMT+0700 (Indochina Time)',
-    speaker: 'user',
-    message: 'How many semesters do Ransit have in a year?',
-  },
-  {
-    time: 'Sun Feb 18 2024 18:35:54 GMT+0700 (Indochina Time)',
-    speaker: 'bot',
-    message: 'Hi, How can I assit you today?',
-  },
-  {
-    time: 'Sun Feb 18 2024 18:37:47 GMT+0700 (Indochina Time)',
-    speaker: 'user',
-    message: 'I need some help!',
-  },
-  {
-    time: 'Sat Feb 18 2024 18:38:20 GMT+0700 (Indochina Time)',
-    speaker: 'bot',
-    message: 'Sure, let me know!',
-  },
-  {
-    time: 'Sat Feb 18 2024 18:39:29 GMT+0700 (Indochina Time)',
-    speaker: 'user',
-    message: 'How many semesters do Ransit have in a year?',
-  },
 ];
--- app/utils/gemini-service.js
+++ app/utils/gemini-service.js
@@ -1,12 +1,10 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
-
-const API_KEY = 'AIzaSyBuo-_X5r5eecQZKo61t0Sb0_liAWC9eRQ';
+import { API_KEY } from './api-key';
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
 export async function generateAnswer(modelHistory, question, updateHistory) {
-  // In real project, we have to provide a lot of Q&As to Gemini Api
   const chat = model.startChat({
     history: modelHistory,
     generationConfig: {
